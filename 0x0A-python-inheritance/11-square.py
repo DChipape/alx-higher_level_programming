77c5c13982100f790c510d87bfe59feb44f8c2a8
dchipape <dimakatsosharon94@gmail.com>
2024-02-02 23:44:17 -0800

    task

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,24 @@
- a Rectangle subclass Square."""
+#!/usr/bin/python3
+
+""" Class that inherits from Rectangle """
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """ Class that defines a Square by inheritance of Rectangle class """
 
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+        """ Constructor """
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Area method through Super call """
+        return super().area()
+
+    def __str__(self):
+        """ Method for when print is used """
+        msg = "[Square] {:d}/{:d}".format(self.__size, self.__size)
+        return msg
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
-
+    """ Geometry Class """
     def area(self):
-        """Not yet implemented."""
+        """ Not Yet Implemented """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+        """ Validates Value
 
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
+        name is assumed to be a string"""
+
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            msg = "{:s} must be an integer".format(name)
+            raise TypeError(msg)
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            msg = "{:s} must be greater than 0".format(name)
+            raise ValueError(msg)
